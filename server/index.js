be22cb8729c4a059acd64e870f7e5b89c89dd566
Ankita Aggarwal <ankita.agg0407@gmail.com>
2026-05-01 16:27:34 +0530

    force cors headers

--- server/index.js
+++ server/index.js
@@ -11,12 +11,19 @@ const dashboardRoutes = require('./routes/dashboard');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
